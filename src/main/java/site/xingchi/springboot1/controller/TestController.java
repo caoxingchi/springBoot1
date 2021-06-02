f20b82589f157de27e6ccb666d17075f99534166
itcolors <caoxingchi@outlook.com>
2021-06-02 23:35:11 +0800

    getName controller

--- src/main/java/site/xingchi/springboot1/controller/TestController.java
+++ src/main/java/site/xingchi/springboot1/controller/TestController.java
@@ -1,5 +1,6 @@
 package site.xingchi.springboot1.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,5 +12,10 @@ public class TestController {
     @PostMapping("/tt")
     public AjaxResult test(){
         return AjaxResult.success("成功","hide");
+    }
+
+    @GetMapping("/getName")
+    public AjaxResult getName(){
+        return AjaxResult.success("获取成功","name:===xingchi");
     }
 }
