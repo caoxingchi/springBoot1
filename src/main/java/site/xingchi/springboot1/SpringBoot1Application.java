819cb910aa69425eb941f0b0b7f0736a297da8fe
itcolors <caoxingchi@outlook.com>
2021-06-03 12:16:47 +0800

    配置文件

--- src/main/java/site/xingchi/springboot1/SpringBoot1Application.java
+++ src/main/java/site/xingchi/springboot1/SpringBoot1Application.java
@@ -2,7 +2,9 @@ package site.xingchi.springboot1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class SpringBoot1Application {
 
