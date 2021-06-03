9fd2b0b50878e04535ae2da7f4d9d18eb02ea837
itcolors <caoxingchi@outlook.com>
2021-06-03 16:01:09 +0800

    测试自动刷新配置文件

--- src/main/java/site/xingchi/springboot1/controller/TestController.java
+++ src/main/java/site/xingchi/springboot1/controller/TestController.java
@@ -1,5 +1,7 @@
 package site.xingchi.springboot1.controller;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,14 +12,20 @@ import site.xingchi.springboot1.utils.AjaxResult;
 @RestController
 @RequestMapping("/test")
 @RefreshScope
+@Slf4j
 public class TestController {
+
+    @Value("${test.name}")
+    private String name;
     @PostMapping("/tt")
     public AjaxResult test(){
+        log.info("name=={}",name);
         return AjaxResult.success("成功","hide");
     }
 
     @GetMapping("/getName")
     public AjaxResult getName(){
+        log.info("name=={}",name);
         return AjaxResult.success("获取成功","name:===xingchi");
     }
 }
